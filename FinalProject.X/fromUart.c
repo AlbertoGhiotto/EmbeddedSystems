cdf05e4a164d486cbf2bdbc1be1ac3a6877c6267
Alberto <alberto.ghiotto@hotmail.it>
2020-03-02 16:37:43 +0100

    From uart source and header

---
+++ FinalProject.X/fromUart.c
@@ -0,0 +1,18 @@
+/*
+ * File:   fromUart.c
+ * Author:   Filippo Gandolfi    S4112879
+ *           Alberto Ghiotto     S4225586
+ *
+ * Created on 28 December 2019, 10:47
+ */
+
+
+#include "xc.h"
+#include "global.h"
+#include "fromUart.h"
+
+
+
+int main(void) {
+    return 0;
+}
---
+++ FinalProject.X/fromUart.h
@@ -0,0 +1,7 @@
+#ifndef XC_HEADER_FROMUART_H
+#define	XC_HEADER_FROMUART_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+#endif	/* XC_HEADER_FROMUART_H */
+
