34ecae0e44160bbec0cba0431a78bbe763aa6901
Alberto <alberto.ghiotto@hotmail.it>
2020-03-03 16:58:32 +0100

    Updating fromUart. Done with decoding messages, still have to send them to pc

--- FinalProject.X/fromUart.c
+++ FinalProject.X/fromUart.c
@@ -10,9 +10,78 @@
 #include "xc.h"
 #include "global.h"
 #include "fromUart.h"
+#include "lcd.h"
+#include "pwm.h"
+#include "parser.h"
 
-
-
-int main(void) {
+int fromUart(void) {
+    
+    while (U2STAbits.URXDA == 1) {  // Indicates that the receive buffer has data available
+            tempVar = U2RXREG;      // Read receive buffer
+            // U2TXREG = tempBuff;
+            tempConv = tempVar;     // Convert int into corresponding char ascii code
+            parseFlag = parse_byte(&pstate, tempConv);
+           
+            if(parseFlag == NEW_MESSAGE){
+                decodeFlag = decodeMessage(pstate.msg_type, pstate.msg_payload);
+            }
+            
+        }
+        if (U2STAbits.URXDA == 1) {  // reset URXDA flag
+            tempVar = U2RXREG;
+        }
+    
+    
     return 0;
+}
+int decodeMessage(char* msg_type, char* msg_payload)
+{   
+    if(strcmp(msg_type, "HLREF") == 0)              // Message is of type HLREF
+    {         
+        if(board_state != STATE_SAFE )
+        {
+            if(board_state == STATE_TIMEOUT)    
+            {
+                // A new message (reference) arrived -> get out of timeout state
+                board_state = STATE_CONTROLLED; 
+            }
+            // Construct the message with the rpm
+            sscanf(msg_payload, "%d,%d", &tempRPM1, &tempRPM2);
+            if(!normalizeDC(&tempRPM1, &tempRPM2))
+            {
+                actualRPM1 = tempRPM1;
+                actualRPM2 = tempRPM2;
+                
+                return REF_P;       // Positive reference ack signal
+            }
+            else    // If the message didn't get sent correctly
+            {
+                return REF_N;       // Negative reference ack signal
+            }
+        }
+        return REF_N;               // In safe mode no new messages are accepted    
+    }
+    else if(strcmp(msg_type, "HLSAT") == 0)             // Message is of type HLSAT
+    {
+        // Construct the message with the rpm
+        sscanf(msg_payload, "%d,%d", &tempMin, &tempMin);
+        if(!updateRange(actualRPM1, actualRPM2))
+        {
+            return SAT_P;           // Positive saturation ack signal
+        }
+        return SAT_N;               // Negative saturation ack signal
+    }
+    else if (strcmp(msg_type, "HLENA") == 0) // Message is of type HLENA
+    {
+        if (board_state == STATE_SAFE) 
+        {
+            board_state = STATE_CONTROLLED;
+            return ENA_P;
+        }
+        else
+        {
+            return ENA_N;
+        }
+    }   
+    return ERR;
 }
--- FinalProject.X/fromUart.h
+++ FinalProject.X/fromUart.h
@@ -3,5 +3,42 @@
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 
+#define DIMENSION 25
+
+// Definition for acknowledgement message type
+#define ERR 0
+
+#define REF_N 1     // for a negative ack to a reference command
+#define REF_P 2     // for a positive ack to a reference command 
+
+#define SAT_N 3     // for a negative ack to a saturation command
+#define SAT_P 4     // for a positive ack to a saturation command
+
+#define ENA_N 5     // for a negative ack to a enabling command
+#define ENA_P 6     // for a positive ack to a enabling command
+
+
+int tempVar = 0;      // Variable for reading uart msgs
+char tempConv;
+
+// Decoding support variables
+int parseFlag = 0;
+int decodeFlag = 0;
+int bufferFlag = 0;
+
+// Support rpm values
+int tempRPM1 = 0;
+int tempRPM2 = 0;
+// Support saturation values
+int tempMin = 0;
+int tempMax = 0;
+
+// Main function
+int fromUart(void);
+
+// Support function to decode messages
+int decodeMessage(char* msg_type, char* msg_payload);
+
+
 #endif	/* XC_HEADER_FROMUART_H */
 
