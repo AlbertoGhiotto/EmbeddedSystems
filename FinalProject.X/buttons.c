4b68eb23fede5eea539b1554d5a13b23a4f8cdd2
Alberto <alberto.ghiotto@hotmail.it>
2020-03-14 17:05:09 +0100

    Updated comment

--- FinalProject.X/buttons.c
+++ FinalProject.X/buttons.c
@@ -13,8 +13,8 @@
 
 // S5 button ISR
 void __attribute__((__interrupt__, __auto_psv__)) _INT0Interrupt() {
-    IFS0bits.INT0IF = 0; // reset interrupt flag
-    IEC0bits.INT0IE = 0; // Disable interrupt of button s5
+    IFS0bits.INT0IF = 0;             // Reset interrupt flag
+    IEC0bits.INT0IE = 0;             // Disable interrupt of button s5
     
     // Stop motors
     int rpm1 = 0;
@@ -28,52 +28,52 @@ void __attribute__((__interrupt__, __auto_psv__)) _INT0Interrupt() {
     // Stop timeout mode timer
     TMR2 = 0;
     T2CONbits.TON = 0;
-    IEC0bits.T2IE = 0; //disable interrupt for timeout mode
-    IFS0bits.T2IF = 0; //set the flag = 0
+    IEC0bits.T2IE = 0;              // Disable interrupt for timeout mode
+    IFS0bits.T2IF = 0;              // Set the flag = 0
     
-    tmr3_setup_period(50);   // Start debouncing timer
+    tmr3_setup_period(50);          // Start debouncing timer
 }
 
 // S6 buttons ISR
 void __attribute__((__interrupt__, __auto_psv__)) _INT1Interrupt(){
-    IFS1bits.INT1IF = 0;        // reset interrupt flag
-    IEC1bits.INT1IE = 0;        // Disable interrupt of button s5
+    IFS1bits.INT1IF = 0;            // Reset interrupt flag
+    IEC1bits.INT1IE = 0;            // Disable interrupt of button s5
     
     // Toggle state of the flag
     flagS6 = (flagS6 + 1 ) % 2;     // If flag = 0 -> flag = 1. If flag = 1 -> flag = 0
     
-    tmr3_setup_period(50);       // Start debouncing timer - previously was 15 but sometimes it still bounced
+    tmr3_setup_period(50);          // Start debouncing timer - previously was 15 but sometimes it still bounced
 }
 
 // Debouncing timer ISR
 void __attribute__((__interrupt__, __auto_psv__)) _T3Interrupt() {
-    IFS0bits.T3IF = 0; // reset interrupt flag of timer 3
+    IFS0bits.T3IF = 0;              // Reset interrupt flag of timer 3
     
-    IFS0bits.INT0IF = 0; // reset interrupt flag of button s5
-    IEC0bits.INT0IE = 1; // Enable interrupt of button s5
+    IFS0bits.INT0IF = 0;            // Reset interrupt flag of button s5
+    IEC0bits.INT0IE = 1;            // Enable interrupt of button s5
     
-    IFS1bits.INT1IF = 0; // reset interrupt flag of button s6
-    IEC1bits.INT1IE = 1; // Enable interrupt of button s6
+    IFS1bits.INT1IF = 0;            // Reset interrupt flag of button s6
+    IEC1bits.INT1IE = 1;            // Enable interrupt of button s6
     
-    T3CONbits.TON = 0; // Stop debouncing timer
+    T3CONbits.TON = 0;              // Stop debouncing timer
 }
 
 // Debouncing timer
 void tmr3_setup_period(int ms){
-    TMR3 = 0; // reset timer counter
+    TMR3 = 0;                       // Reset timer counter
     Fcy = (Fosc / 4.0);
     PR3 = (Fcy) / 64.0 * (ms / 1000.0);
 
-    T3CONbits.TCKPS = 0b10; // prescaler 1:64    -> up to a bit more than 2 seconds
-    T3CONbits.TON = 1; // starts the timer!
+    T3CONbits.TCKPS = 0b10;         // Prescaler 1:64    -> up to a bit more than 2 seconds
+    T3CONbits.TON = 1;              // Starts the timer!
 }
 
 // Function enabling flag of buttons
 void setButton(){
-    IEC0bits.T3IE = 1; // Enable interrupt of debouncing timer t3
+    IEC0bits.T3IE = 1;              // Enable interrupt of debouncing timer t3
         
-    IFS0bits.INT0IF = 0; //reset interrupt flag for S5 button
-    IFS1bits.INT1IF = 0; //reset interrupt flag for S6 button
-    IEC0bits.INT0IE = 1; //enable interrupt for S5 button
-    IEC1bits.INT1IE = 1; //enable interrupt for S6 button
+    IFS0bits.INT0IF = 0;            // Reset interrupt flag for S5 button
+    IFS1bits.INT1IF = 0;            // Reset interrupt flag for S6 button
+    IEC0bits.INT0IE = 1;            // Enable interrupt for S5 button
+    IEC1bits.INT1IE = 1;            // Enable interrupt for S6 button
 }
